4ff8e1478a820cc663b4813008c3ec3801bbc4ce
KIMGWANYEONG <pipe0youngk@gmail.com>
2024-05-13 15:38:29 +0900

    second

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <header className="bg-blue-100 px-2 py-4 flex justify-between">
       <Logo />
-      <div className="flex place-items-center gap-4">
+      <div className="flex items-center gap-4">
         <NavBar />
         <Button name="..." isMoreButton={true} />
         <WeatherIcon />
