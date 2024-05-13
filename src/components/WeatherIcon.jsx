16fc71d54b76ea0fad1b02f66df945bab25db81b
KIMGWANYEONG <pipe0youngk@gmail.com>
2024-05-13 15:36:21 +0900

    weather-api

---
+++ src/components/WeatherIcon.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useEffect } from "react";
+
+const WeatherIcon = () => {
+  const [latitude, setLatitude] = useState();
+  const [longitude, setLongitude] = useState();
+
+  const getWeather = async () => {
+    try {
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    });
+  }, []);
+
+  return <div>WeatherIcon</div>;
+};
+
+export default WeatherIcon;
